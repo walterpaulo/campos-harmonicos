111ccd8765461b16a38c52718c94dba729df0d24
walterpaulo <walter0paulo@hotmail.com>
2023-02-26 16:42:38 -0300

    init

---
+++ js/script.js
@@ -0,0 +1,110 @@
+var tons = [];
+var notas = [{ titulo: "C", descricao: "Dó Menor", dataDescricao: "Primeiro Grau", grau: "primeira-escala" },
+             { titulo: "Dm", descricao: "Ré Menor", dataDescricao: "Segundo Grau", grau: "segunda-escala" },
+             { titulo: "Em", descricao: "Mí Menor", dataDescricao: "Terceiro Grau", grau: "terceira-escala" },
+             { titulo: "F", descricao: "Fá Menor", dataDescricao: "Quarto Grau", grau: "quarta-escala" },
+             { titulo: "G", descricao: "Sól Menor", dataDescricao: "Quinto Grau", grau: "quinta-escala" },
+             { titulo: "Am", descricao: "Lá Menor", dataDescricao: "Sexto Grau", grau: "sexta-escala" },
+             { titulo: "Bº", descricao: "Sí Bemol", dataDescricao: "Setima Grau", grau: "setima-escala" }];
+tons.push(notas)
+
+notas = [{ titulo: "D", descricao: "Dó Menor", dataDescricao: "Primeiro Grau", grau: "primeira-escala" },
+             { titulo: "Em", descricao: "Mí Menor", dataDescricao: "Segundo Grau", grau: "segunda-escala" },
+             { titulo: "F#m", descricao: "Fá Sustenido Menor", dataDescricao: "Terceiro Grau", grau: "terceira-escala" },
+             { titulo: "G", descricao: "Só", dataDescricao: "Quarto Grau", grau: "quarta-escala" },
+             { titulo: "A", descricao: "Lá", dataDescricao: "Quinto Grau", grau: "quinta-escala" },
+             { titulo: "Bm", descricao: "Sí Menor", dataDescricao: "Sexto Grau", grau: "sexta-escala" },
+             { titulo: "C#º", descricao: "Dó Sustenido Bemol", dataDescricao: "Setima Grau", grau: "setima-escala" }];
+tons.push(notas)
+
+notas = [{ titulo: "E", descricao: "Mí", dataDescricao: "Primeiro Grau", grau: "primeira-escala" },
+             { titulo: "F#m", descricao: "F Sustenido Menor", dataDescricao: "Segundo Grau", grau: "segunda-escala" },
+             { titulo: "G#m", descricao: "Sól Sustenido Menor", dataDescricao: "Terceiro Grau", grau: "terceira-escala" },
+             { titulo: "A", descricao: "Lá", dataDescricao: "Quarto Grau", grau: "quarta-escala" },
+             { titulo: "B", descricao: "Sí", dataDescricao: "Quinto Grau", grau: "quinta-escala" },
+             { titulo: "C#m", descricao: "Dó Sustenido Menor", dataDescricao: "Sexto Grau", grau: "sexta-escala" },
+             { titulo: "D#º", descricao: "Ré Sustenido Bemol", dataDescricao: "Setima Grau", grau: "setima-escala" }];
+tons.push(notas)
+
+notas = [{ titulo: "F", descricao: "Fá", dataDescricao: "Primeiro Grau", grau: "primeira-escala" },
+             { titulo: "Gm", descricao: "Sól", dataDescricao: "Segundo Grau", grau: "segunda-escala" },
+             { titulo: "Am", descricao: "Lá Menor", dataDescricao: "Terceiro Grau", grau: "terceira-escala" },
+             { titulo: "Bb", descricao: "Sí Bemol", dataDescricao: "Quarto Grau", grau: "quarta-escala" },
+             { titulo: "C", descricao: "Dó", dataDescricao: "Quinto Grau", grau: "quinta-escala" },
+             { titulo: "Dm", descricao: "Ré Menor", dataDescricao: "Sexto Grau", grau: "sexta-escala" },
+             { titulo: "Eº", descricao: "Sí Bemol", dataDescricao: "Setimo Grau", grau: "setima-escala" }];
+tons.push(notas)
+
+notas = [{ titulo: "G", descricao: "Sól", dataDescricao: "Primeiro Grau", grau: "primeira-escala" },
+             { titulo: "Am", descricao: "Lá menor", dataDescricao: "Segundo Grau", grau: "segunda-escala" },
+             { titulo: "Bm", descricao: "Sí Menor", dataDescricao: "Terceiro Grau", grau: "terceira-escala" },
+             { titulo: "C", descricao: "Dó", dataDescricao: "Quarto Grau", grau: "quarta-escala" },
+             { titulo: "D", descricao: "Ré", dataDescricao: "Quinto Grau", grau: "quinta-escala" },
+             { titulo: "Em", descricao: "Mí Menor", dataDescricao: "Sexto Grau", grau: "sexta-escala" },
+             { titulo: "F#º", descricao: "Fá Sustemido Bemol", dataDescricao: "Setimo Grau", grau: "setima-escala" }];
+tons.push(notas)
+
+notas = [{ titulo: "A", descricao: "Lá", dataDescricao: "Primeiro Grau", grau: "primeira-escala" },
+             { titulo: "Bm", descricao: "Sí menor", dataDescricao: "Segundo Grau", grau: "segunda-escala" },
+             { titulo: "C#m", descricao: "Dó Sustenido Menor", dataDescricao: "Terceiro Grau", grau: "terceira-escala" },
+             { titulo: "D", descricao: "Ré", dataDescricao: "Quarto Grau", grau: "quarta-escala" },
+             { titulo: "E", descricao: "Mí", dataDescricao: "Quinto Grau", grau: "quinta-escala" },
+             { titulo: "F#m", descricao: "Fá Sustenido Menor", dataDescricao: "Sexto Grau", grau: "sexta-escala" },
+             { titulo: "G#º", descricao: "Sól Sustenido Bemol", dataDescricao: "Setima Grau", grau: "setima-escala" }];
+tons.push(notas)
+
+notas = [{ titulo: "B", descricao: "Sí", dataDescricao: "Primeiro Grau", grau: "primeira-escala" },
+             { titulo: "C#m", descricao: "Dó Sustenido menor", dataDescricao: "Segundo Grau", grau: "segunda-escala" },
+             { titulo: "D#m", descricao: "Ré Sustenido Menor", dataDescricao: "Terceiro Grau", grau: "terceira-escala" },
+             { titulo: "E", descricao: "Mí", dataDescricao: "Quarto Grau", grau: "quarta-escala" },
+             { titulo: "F", descricao: "Fá", dataDescricao: "Quinto Grau", grau: "quinta-escala" },
+             { titulo: "Gm", descricao: "Sól Menor", dataDescricao: "Sexto Grau", grau: "sexta-escala" },
+             { titulo: "A#º", descricao: "Lá Sustenido Bemol", dataDescricao: "Setima Grau", grau: "setima-escala" }];
+tons.push(notas)
+
+var notas = [{ titulo: "C", descricao: "Dó Menor", dataDescricao: "Primeiro Grau", grau: "primeira-escala" },
+             { titulo: "Dm", descricao: "Ré Menor", dataDescricao: "Segundo Grau", grau: "segunda-escala" },
+             { titulo: "Em", descricao: "Mí Menor", dataDescricao: "Terceiro Grau", grau: "terceira-escala" },
+             { titulo: "F", descricao: "Fá Menor", dataDescricao: "Quarto Grau", grau: "quarta-escala" },
+             { titulo: "G", descricao: "Sól Menor", dataDescricao: "Quinto Grau", grau: "quinta-escala" },
+             { titulo: "Am", descricao: "Lá Menor", dataDescricao: "Sexto Grau", grau: "sexta-escala" },
+             { titulo: "Bº", descricao: "Sí Bemol", dataDescricao: "Setima Grau", grau: "setima-escala" }];
+tons.push(notas)
+
+function carregarDados([]) {
+
+    tabela = document.getElementById("harmonico-table");
+
+    tons.forEach(notas => {
+        notas.forEach(nota => {
+                // titulo
+            const titulo = document.createElement('div');
+            titulo.setAttribute('class', 'title');
+            const text = document.createTextNode(nota.titulo)
+            titulo.appendChild(text)
+
+            // descricao
+            const descricao = document.createElement('div');
+            descricao.setAttribute('class', 'description');
+            const text2 = document.createTextNode(nota.descricao)
+            descricao.appendChild(text2)
+            
+            // 1
+            const campoHarmonicoInner = document.createElement('div');
+            campoHarmonicoInner .setAttribute('class', 'campo-harmonico-inner');
+            campoHarmonicoInner.insertAdjacentElement('beforeend', titulo)
+            campoHarmonicoInner.insertAdjacentElement('beforeend', descricao)
+
+            // tipo de escala
+            const campoHarmonico = document.createElement('div');
+            campoHarmonico.setAttribute('class', 'campo-harmonico '+nota.grau);
+            campoHarmonico.setAttribute('data-description', nota.dataDescricao);
+
+            // adicionar na tabela
+            campoHarmonico.insertAdjacentElement('beforeend', campoHarmonicoInner)
+            tabela.insertAdjacentElement('beforeend',campoHarmonico)
+        })
+    });
+}
+
+this.carregarDados(tons)
